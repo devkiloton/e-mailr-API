c3d578767f7c0fddf36ddd5d2f157da95faa5060
devkiloton <dev.kiloton@gmail.com>
2022-07-02 12:52:05 -0300

    configuring eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,23 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'google'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'require-jsdoc': 0,
+    semi: 0,
+    indent: ['error', 2],
+    'comma-dangle': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
   },
 };
