d1f3393dfc46f8e837681c39d299d8c93a94b4b6
devkiloton <dev.kiloton@gmail.com>
2022-07-27 12:29:14 -0300

    Send Email

---
+++ src/mail/mail.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param} from '@nestjs/common';
+import { MailService } from './mail.service';
+
+@Controller('mail')
+export class MailController {
+
+    constructor(private readonly mailService: MailService) {}
+
+    @Get(':email/:name')
+    signUp(@Param('email') email:string, @Param('name') name:string) {
+        console.log(email, name)
+        return this.mailService.sendUserConfirmation(email, name);
+    }
+}
---
+++ src/mail/mail.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+
+@Injectable()
+export class MailService {
+
+    constructor(private mailerService: MailerService) {}
+
+    async sendUserConfirmation(receiver:string, name: string) {
+        await this.mailerService.sendMail({
+            to: receiver,
+            subject: `${name}, bem vindo à Xarlat!`,
+            template: "./confirmation", 
+            context: { 
+                name: name,
+                url: "xarlat.com"
+            },
+        });
+    }
+}
